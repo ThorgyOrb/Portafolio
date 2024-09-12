03047d75ff8dc88b7ceb0ed7114ea9b8428f1dcc
ThorgyOrb <112546683+ThorgyOrb@users.noreply.github.com>
2024-09-12 13:22:03 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
