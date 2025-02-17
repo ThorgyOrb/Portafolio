49caaa81e60b1ac100574b36e4fc7f7e4a223d4d
ThorgyOrb <112546683+ThorgyOrb@users.noreply.github.com>
2025-02-17 00:07:20 -0600

    v2

--- src/pages/PDFSplit.jsx
+++ src/pages/PDFSplit.jsx
@@ -5,8 +5,8 @@ const projectData = {
   title: 'Escaneo de Facturas',
   description: 'Separador de facturas',
   details: 'Aplicación web diseñada para cargar un compendio de facturas en formato PDF, analizar cada página de forma individual y separar automáticamente cada factura. Al finalizar el proceso, genera un archivo ZIP con todas las facturas organizadas por separado.',
-  technologies: ['ASP.NET MVC', 'HTML5', 'CSS3', 'JavaScript', 'Fetch API', 'Bootstrap', 'SQL Server', 'Visual Studio', 'Git', 'AJAX' ],
-  achievements: 'El principal reto del proyecto fue superar las limitaciones de escalabilidad de la aplicación original. Aunque la aplicación existente ofrecía las funcionalidades necesarias, no era capaz de adaptarse a las crecientes demandas de gestión de facturas en diversas plantas. \n\n  Para superar estos desafíos, se emprendió una reestructuración completa de la aplicación. El nuevo diseño no solo abordó los problemas de escalabilidad, sino que también integró capacidades mejoradas para manejar la gestión de facturas de manera más eficiente. Esta renovación resultó en una mejora del rendimiento del sistema, con una optimización en los tiempos de carga y una reducción en el uso de recursos, superando las expectativas y estableciendo una base sólida para futuros desarrollos.',
+  technologies: ['ASP.NET MVC', 'HTML5', 'CSS3', 'JavaScript', 'Fetch API', 'Bootstrap', 'Visual Studio', 'Git' ],
+  achievements: 'El desafío principal de este proyecto consistió en encontrar una forma efectiva de leer los archivos PDF y, al mismo tiempo, extraer los códigos QR presentes en ellos. Además, debido a que algunos documentos tenían una calidad deficiente, se implementó un sistema de mejora de contraste para optimizar la legibilidad de los QR, asegurando que pudieran ser correctamente detectados incluso en condiciones de mala calidad.',
   images: [
     { src: '/Portafolio/assets/pictures/PDFSplit/pdf1.png', alt: 'Captura de Pantalla 1', description: 'Pantalla principal, descargar el archivo.' },
     { src: '/Portafolio/assets/pictures/PDFSplit/pdf2.png', alt: 'Captura de Pantalla 2', description: 'Pantalla principal, cargar el archivo.' },
